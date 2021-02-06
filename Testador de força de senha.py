c871abec65159511715d106de5a1599e1a3dc3d4
Suu Kirinus Nogueira <50893598+Suu021@users.noreply.github.com>
2021-02-06 18:32:41 -0300

    Programinha que simula o tempo que um keylogger levaria pra descobrir a sua senha.

---
+++ Testador de força de senha.py
@@ -0,0 +1,26 @@
+def testador(key):
+    import random
+    import string
+
+    letras = string.ascii_uppercase
+    num_letras = len(key)
+
+    codigo = ''.join(random.choice(letras) for _ in range(num_letras))
+    while codigo != key:
+        codigo = ''.join(random.choice(letras) for _ in range(num_letras))
+        if codigo == key:
+            return codigo
+
+
+def tempo_quebrar_senha(teste):
+    from timeit import timeit
+
+    tempo = timeit('teste', 'from __main__ import teste', number=1)
+    return tempo
+
+
+senha = input(print("Digite a senha que deseja testar: "))
+teste = testador(senha)
+tempo_teste = tempo_quebrar_senha(teste)
+print(teste)
+print("O keylogger levaria aproximadamente ", tempo_teste, "segundos pra quebrar a sua senha")
