67ba2a005a4aecff9d048e0a603829ebec69834d
Suu Kirinus Nogueira <50893598+Suu021@users.noreply.github.com>
2021-02-12 13:32:09 -0300

    Troquei a bilbioteca random pela secrets

--- Gerador de senhas.py
+++ Gerador de senhas.py
@@ -1,11 +1,13 @@
 def gerador_senha (key):
-    import random
+    import secrets
     import string
 
     todos_caract = string.ascii_uppercase+string.ascii_lowercase+string.digits+string.punctuation
-    codigo = ''.join(random.choice(todos_caract) for _ in range(key))
+    codigo = ''.join(secrets.choice(todos_caract) for _ in range(key))
     return codigo
 
 num = int(input("Digite o número de caracteres que deseja pra sua senha: "))
 senha = gerador_senha(num)
 print("Sua nova senha é: ", senha)
+
+
