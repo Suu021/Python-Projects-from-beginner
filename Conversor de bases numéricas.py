1b79bb0bc143d4a8bd1b59ae45aa8639b7528332
Suu Kirinus Nogueira <50893598+Suu021@users.noreply.github.com>
2021-02-16 12:05:49 -0300

    Adicionei opção para Converter números decimais para octais

--- Conversor de bases numéricas.py
+++ Conversor de bases numéricas.py
@@ -9,6 +9,26 @@ def binario(num):
     n_bin = "".join(lista_b)
     return n_bin[::-1]
 
+def octal(num):
+    lista_oc = []
+    while num >= 8:
+        div = num % 8
+        lista_oc.append(str(div))
+        octal = "".join(lista_oc)
+        num = num // 8
+    lista_oc.append(str(num))
+    octal = "".join(lista_oc)
+    print(octal[::-1])
 
 num = int(input("Qual número quer converter? "))
-print(binario(num))
+menu = input("Menu:"
+              "\n(a)Binário;"
+              "\n(b)octal;"
+             "\npara qual sistema deseja converter? ")
+
+if menu is "a":
+    print(binario(num))
+elif menu is "b":
+    print(octal(num))
+else:
+    print("Opção inválida!")
