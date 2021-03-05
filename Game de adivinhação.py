7b9cf56fd01ab9edbb244ca038d3fab63f43ce88
Suu Kirinus Nogueira <50893598+Suu021@users.noreply.github.com>
2021-03-05 09:01:58 -0300

    Melhorei o game

--- Game de adivinhação.py
+++ Game de adivinhação.py
@@ -1,15 +1,21 @@
 from random import randint
 from time import sleep
 
-num_sorteado = randint(0, 9)
-print("-=-" * 20)
-print("Vou pensar em um número de 0 a 9. Tente adivinhar...")
-print("-=-" * 20)
-sleep(1)
-num = int(input("E então, qual o número que eu pensei? "))
-print("PROCESSANDO...")
-sleep(3)
-if num == num_sorteado:
-    print("Parabéns, você acertou!")
-else:
-    print("Você errou, o número era: ", num_sorteado)
+vitoria = False
+cont_derrotas = 0
+while not vitoria:
+    num_sorteado = randint(0, 9)
+    print("-=-" * 20)
+    print("Vou pensar em um número de 0 a 9. Tente adivinhar...")
+    print("-=-" * 20)
+    sleep(1)
+    num = int(input("E então, qual o número que eu pensei? "))
+    print("PROCESSANDO...")
+    sleep(3)
+    if num == num_sorteado:
+        print("Parabéns, você acertou!")
+        vitoria = True
+    else:
+        print("Você errou, o número era: ", num_sorteado)
+        cont_derrotas += 1
+print("Você acertou na {}ª tentativa!".format(cont_derrotas+1))
