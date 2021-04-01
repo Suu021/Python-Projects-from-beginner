7d7b08fc37c149428b208217a3ba2a0b44bd969f
Suu Kirinus Nogueira <50893598+Suu021@users.noreply.github.com>
2021-04-01 13:34:45 -0300

    Corrigi o bug do código

--- Busca por Brute Force.py
+++ Busca por Brute Force.py
@@ -1,20 +1,28 @@
-Arquivo = input("Insira uma lista em formato .txt:")
-Lista = []
+from colorama import Fore
+
+
+print("=-" * 20)
+print(f"{'Busca de palavra-chave':^20}")
+print("=-" * 20)
+
+Arquivo = input('Insira uma lista em formato "*.txt": ')
+cont = 0
+lista = []
 #Exemplo: C:\\Users\\Suu\\Documents\\Lista de frutas.txt
 #C:\\Users\\Suu\\Documents\\Lista de games.txt
 
 with open(Arquivo, 'r', encoding='UTF-8') as f:
-    Dados = f.readlines()
-    for linha in Dados:
-        for i in linha.split(','):
-            Lista.append(i)
-            print(Lista)
+    dados = f.readlines()
+    for linha in dados:
+        for i in linha.split(', '):
+            lista.append(i)
 
-Keyword = input("Dê uma palavra-chave: ")
+Keyword = input("Dê uma palavra-chave: ").strip().capitalize()
 
-for item in Lista:
-    print(item, Lista.index(item))
-    if item != Keyword:
-        print('A palavra-chave não está na lista')
-    else:
-        print("Foi encontrada a palavra-chave na lista, na posição: ", Lista.index(Keyword))
+for item in lista:
+    if item.lower() == Keyword.lower():
+        print(Fore.BLUE + f'"{Keyword}" está na {lista.index(Keyword)}ª posição da lista!' + Fore.RESET)
+        cont += 1
+        break
+if cont == 0:
+    print(Fore.RED + f'Infelizmente "{Keyword}" não está na lista...' + Fore.RESET)
