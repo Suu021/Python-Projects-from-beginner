1b478f1ae47de50691eaa5298e2052b797bee6bc
Suu Kirinus Nogueira <50893598+Suu021@users.noreply.github.com>
2021-04-14 19:53:57 -0300

    adicionei o Morse_Decoder ao Project-tests

--- Project-tests.py
+++ Project-tests.py
@@ -2,6 +2,8 @@ from unittest import TestCase, main
 from re import compile
 import Tabuada
 import Gerador_de_senhas
+from Morse_Decoder import decodeMorse
+
 
 class MyTestCase_Tabuada(TestCase):
     def test_entrada_número_saída_multiplo_de_número(self):
@@ -15,9 +17,16 @@ class MyTestCase_Gerador_de_senhas(TestCase):
     def test_all_characters_numerics_and_special_characters(self):
         regex = compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[ -\/:-@\[-`{-~]).+$')
 
-        #for i in range(100):
-            #with self.subTest(i=i):
+        # for i in range(100):
+        # with self.subTest(i=i):
         self.assertRegex(Gerador_de_senhas.gerar_senha(16), regex)
+
+
+class MyTestCase_Morse_Decoder(TestCase):
+    def text_decode_of_morse_codes(self):
+        self.assertEqual("---   .-.  .-  -  ---   .-.  ---  .  ..-   .-   .-.  ---  ..-  .--.  .-   -..  ---   .-.  . "
+                         " ..   -..  .   .-.  ---  --  .-", "O RATO ROEU A ROUPA DO REI DE ROMA")
+        self.assertEqual(".... -.. ....- ---.. ...-- --...", "HD4837")
 
 
 if __name__ == '__main__':
